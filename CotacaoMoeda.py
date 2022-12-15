1b0d4cf715cae859f0e16d4f1066b9dacfa7bc9b
Daniel Silveira <murtdaniel@gmail.com>
2022-12-15 11:52:53 -0300

    Commit Inicial

---
+++ CotacaoMoeda.py
@@ -0,0 +1,11 @@
+import requests
+import json
+import pprint
+
+cotacoes = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+cotacoes = cotacoes.json()
+cotacao_dolar = cotacoes['USDBRL']['bid']
+print(cotacao_dolar)
+
+
+
