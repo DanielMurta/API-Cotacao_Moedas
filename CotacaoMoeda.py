c45f8194a4d4aa47e2dbf52f8aedc879b6e1e4e6
Daniel Silveira <murtdaniel@gmail.com>
2022-12-16 15:52:21 -0300

    [Criação de interface gráica]-[Adicionando Euro e Bitcoin][Projeto finalizado]

--- CotacaoMoeda.py
+++ CotacaoMoeda.py
@@ -1,11 +1,33 @@
 import requests
 import json
-import pprint
+import PySimpleGUI as sg
 
-cotacoes = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
-cotacoes = cotacoes.json()
-cotacao_dolar = cotacoes['USDBRL']['bid']
-print(cotacao_dolar)
+sg.theme('DarkBlue')
+
+layout = [
+    [sg.Text('Cotação de moedas', font='arial 13')],
+    [sg.Button('Dollar'), sg.Button('Euro'), sg.Button('Bitcoin')],
+    [sg.Text('', key='msg')]
+]
+
+window = sg.Window('Cotação de Moedas', layout=layout, size=(300,100), element_justification='Center')
+
+while True:
+    evento, valores = window.read()
+    if evento == sg.WIN_CLOSED:
+        exit()
+    cotacoes = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+    cotacoes = cotacoes.json()
+    if evento == 'Dollar':
+        cotacao_dolar = float(cotacoes['USDBRL']['bid'])
+        window['msg'].update(f'R${cotacao_dolar:.2f}')
+    elif evento == 'Euro':
+        cotacao_euro = float(cotacoes['EURBRL']['bid'])
+        window['msg'].update(f'R${cotacao_euro:.2f}')
+    elif evento == 'Bitcoin':
+        cotacao_bitcoin = float(cotacoes['BTCBRL']['bid'])
+        window['msg'].update(f'R${cotacao_bitcoin:.2f}')
+
 
 
 
---
+++ README.md
@@ -0,0 +1,1 @@
+API Cotação de Moedas
